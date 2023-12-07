bad14b207a531f664d920a4dcfcc389bf2f41311
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2023-12-07 21:23:52 +0100

    Add Header component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Wagmi from './web3/Wagmi';
+import Header from './web3/Header';
 
 function App() {
   return (
     <Wagmi>
       <div className="flex flex-col h-screen">
+        <Header />
 
         <div className="flex items-center justify-center flex-grow">
           <div className="min-w-[400px] border border-gray-400 rounded">
