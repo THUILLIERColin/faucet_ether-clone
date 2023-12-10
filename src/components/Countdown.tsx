c7121e8059e9cac56f078bc931e7e9072aaf01cd
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2023-12-10 16:16:11 +0100

    Reformat Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 interface CountdownProps {
-    isCounting: boolean;
-    time?: number;
+  isCounting: boolean;
+  time?: number;
 }
 
-const Countdown: React.FC<CountdownProps> = ({ isCounting, time}) => {
-    const [count, setCount] = useState(time || 60);
+const Countdown: React.FC<CountdownProps> = ({ isCounting, time }) => {
+  const [count, setCount] = useState(time || 60);
 
-    useEffect(() => {
-        let interval: NodeJS.Timeout;
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
 
-        if (isCounting) {
-            interval = setInterval(() => {
-                setCount((prevCount) => prevCount - 1);
-            }, 1000 * count);
-        }
+    if (isCounting) {
+      interval = setInterval(() => {
+        setCount((prevCount) => prevCount - 1);
+      }, 1000 * count);
+    }
 
-        return () => {
-            setCount(time || 60);
-            clearInterval(interval);
-        };
-    }, [isCounting]);
+    return () => {
+      setCount(time || 60);
+      clearInterval(interval);
+    };
+  }, [isCounting]);
 
-    return <>{count}</>;
+  return (
+    <p className="mt-4 text-sm text-gray-500">
+      You can request Ether in {count} minutes
+    </p>
+  );
 };
 
 export default Countdown;
