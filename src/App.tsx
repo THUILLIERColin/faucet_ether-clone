39612c1ceac64c9edf230725d614d77776aebfe2
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2023-12-10 11:01:33 +0100

    Refractor the design

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,8 @@ function App() {
       <div className="flex flex-col h-screen">
         <Header />
 
-        <div className="flex items-center justify-center flex-grow">
-          <div className="min-w-[400px] border border-gray-400 rounded">
-            <div className="p-4">
-              <RequestEther />
-            </div>
-          </div>
-        </div>
+        <RequestEther />
+        
       </div>
     </Wagmi>
   );
--- src/web3/RequestEther.tsx
+++ src/web3/RequestEther.tsx
@@ -33,9 +33,8 @@ const RequestEther: FC<RequestEtherProps> = () => {
 
   /* Gestion de la demande d'Ether */
   const handleRequestEther = () => {
-    console.log("Ceci est un clique sur le bouton");
     const existingRecord = clickRecords[ipAddress];
-    const duration = 1; // en minutes
+    const duration = 60; // en minutes
 
     if (existingRecord) {
       // Si l'adresse IP est déjà présente, vérifier la différence de temps
@@ -52,63 +51,73 @@ const RequestEther: FC<RequestEtherProps> = () => {
     }
     setButtonClass("w-full bg-gray-400 cursor-not-allowed");
 
-    setTimeout(() => {
-      setButtonClass("w-full");
-    }, duration * 60 * 1000);
-
     if (isConnected && address) {
-      write?.();
-      console.log("Ceci est une demande d'Ether");
+      try {
+        write?.();
+      }catch(e){
+        console.log(e);
+      }
+      setTimeout(() => {
+        setButtonClass("w-full");
+      }, duration * 60 * 1000);
       addClickRecord(ipAddress, new Date());
     }
   };
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center w-full h-full">
-        {isConnected && address ? (
-          <>
-            <p className="mb-4 text-xl font-bold">Request Ether</p>
-            <p className="mb-4 text-lg">
-              Request Ether from the Rinkeby Faucet for your address{" "}
-              {shortAddr(address)}
-            </p>
-            <Button
-              className={buttonClass}
-              onClick={() => {
-                handleRequestEther();
-              }}
-            >
-              Request Ether
-            </Button>
-          </>
-        ) : (
-          <>
-            <p className="mb-4 text-xl font-bold">Request Ether</p>
-            <p className="mb-4 text-lg">Connect to request Ether</p>
-          </>
-        )}
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Adresse IP</th>
-            <th className="px-4 py-2">Date et heure</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.entries(clickRecords).map(([ip, timestamp]) => (
-            <tr key={ip}>
-              <td className="border px-4 py-2">{ip}</td>
-              <td className="border px-4 py-2">
-                {timestamp.toDateString() +
-                  " " +
-                  timestamp.toLocaleTimeString()}
-              </td>
+    <> 
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Adresse IP</th>
+              <th className="px-4 py-2">Date et heure</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {Object.entries(clickRecords).map(([ip, timestamp]) => (
+              <tr key={ip}>
+                <td className="border px-4 py-2">{ip}</td>
+                <td className="border px-4 py-2">
+                  {timestamp.toDateString() +
+                    " " +
+                    timestamp.toLocaleTimeString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex items-center justify-center flex-grow">
+        <div className="min-w-[400px] border border-gray-400 rounded">
+          <div className="p-4">
+            <div className="flex flex-col items-center justify-center w-full h-full">
+              {isConnected && address ? (
+                <>
+                  <p className="mb-4 text-xl font-bold">Request Ether</p>
+                  <p className="mb-4 text-lg">
+                    Request Ether from the Rinkeby Faucet for your address{" "}
+                    {shortAddr(address)}
+                  </p>
+                  <Button
+                    className={buttonClass}
+                    onClick={() => {
+                      handleRequestEther();
+                    }}
+                  >
+                    Request Ether
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <p className="mb-4 text-xl font-bold">Request Ether</p>
+                  <p className="mb-4 text-lg">Connect to request Ether</p>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
